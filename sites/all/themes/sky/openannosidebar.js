8fc560c32e193246ae8069ea005e37ce250299b3
David Rettenmaier <drett@umd.edu>
2016-03-24 12:47:22 -0400

    Tweaks to js that opens sidebar

--- sites/all/themes/sky/openannosidebar.js
+++ sites/all/themes/sky/openannosidebar.js
@@ -1,6 +1,8 @@
  // Wait for the page to load first
-        window.addEventListener('click', function() {
-        
+ 
+ // Wait for the page to load first
+        window.addEventListener('load',function(){
+        window.addEventListener('mouseover', function() {
 	var $lba= document.querySelectorAll('.annotator-hl');
 	function selectAnnos() {
 		expandoffcanvasmenu('open'); return false;
@@ -8,9 +10,26 @@
 
 // Assign a click event handler to every element:
 	for(var i=0; i<$lba.length; i++) {
-   		$lba[i].onclick = selectAnnos;
+   		$lba[i].addEventListener('click', selectAnnos, false);
 		}
-   
+   }); 
+}); 
+
+/*//window.addEventListener('load',function(){
+        window.addEventListener('click', function() {
+        
+	var $lba= document.querySelectorAll('.annotator-hl');
+	function selectAnnos2() {
+		expandoffcanvasmenu('open'); return false;
+        }
+
+// Assign a click event handler to every element:
+	for(var i=0; i<$lba.length; i++) {
+   		$lba[i].addEventListener('click', selectAnnos2, false);
+		}
+ }); 
+ // }); 
+ */
 
 // Extra function to control menu state from anywhere on your page/ inside your script
 function expandoffcanvasmenu(action){ //param: 'open', 'close', or empty to toggle menu state
@@ -18,4 +37,4 @@ function expandoffcanvasmenu(action){ //param: 'open', 'close', or empty to togg
     var newstate = (action == 'open')? true : (action == 'close')? false : !togglebox.checked
     togglebox.checked = newstate
 }
- });
+ 
