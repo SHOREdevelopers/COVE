4cd6ced517ed539d1b8d7fcafdd86d3f6459d4ed
David Rettenmaier <drett@umd.edu>
2017-01-31 10:08:36 -0500

    Corrected links in OpenID Connect login block

--- sites/all/themes/sky/templates/block--openid_connect--openid_connect_login.tpl.php
+++ sites/all/themes/sky/templates/block--openid_connect--openid_connect_login.tpl.php
@@ -6,7 +6,7 @@
 <?php print render($title_suffix); ?>
 
 <div class="content"<?php print $content_attributes; ?>>
-<nav class="navbar navbar-inverse navbar-fixed-top" id="navbar"> <div class="container"> <nav class="navbar navbar-inverse navbar-fixed-top" id="navbar"> <div class="container"> <ul class="nav navbar-nav"> <li><a class="navbar-link" href="#">COVE Studio <i class="fa fa-external-link-square"></i></a></li> <li class="active"> <a class="navbar-link" target="cove-editions" href="http://dev-rc-distro.pantheonsite.io/">COVE Editions</a> </li> </ul> <ul class="nav navbar-nav pull-right hidden-xs" id="globalnav"> <li>
+<nav class="navbar navbar-inverse navbar-fixed-top" id="navbar"> <div class="container"> <nav class="navbar navbar-inverse navbar-fixed-top" id="navbar"> <div class="container"> <ul class="nav navbar-nav"> <li><a class="navbar-link" href="http://studio.covecollective.org/">COVE Studio <i class="fa fa-external-link-square"></i></a></li> <li class="active"> <a class="navbar-link" target="cove-editions" href="http://live-rc-distro.pantheonsite.io/">COVE Editions</a> </li> </ul> <ul class="nav navbar-nav pull-right hidden-xs" id="globalnav"> <li>
 <?php print $content ?>
 </li> <a href="http://navsa.org"><img src="/sites/default/files/adaptivetheme/sky_files/NAVSAlogo.png" alt="NAVSA logo"/></a> </ul> </div> </nav> </div> </nav>
 </div>
