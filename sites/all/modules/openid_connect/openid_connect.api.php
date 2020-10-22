609d8eab5f1cf1d9c3c50248d2b75fc525d09bcb
David Rettenmaier <drett@umd.edu>
2020-10-22 11:57:30 -0500

    Update to dev branch of OpenID Connect with patch for allowing existing users to connect with other providers provided same email address.

--- sites/all/modules/openid_connect/openid_connect.api.php
+++ sites/all/modules/openid_connect/openid_connect.api.php
@@ -11,6 +11,33 @@
  */
 
 /**
+ * Pre authorize hook that runs before a user is authorized.
+ *
+ * @param array $tokens
+ *   ID token and access token that we received as a result of the OpenID
+ *   Connect flow.
+ * @param object $account
+ *   The user account if it exists, false if not.
+ * @param array $userinfo
+ *   The user claims returned by the OpenID Connect provider.
+ * @param string $client_name
+ *   The machine name of the OpenID Connect client plugin.
+ *
+ * @return bool
+ *   TRUE if user should be logged into Drupal. FALSE if not.
+ */
+function hook_openid_connect_pre_authorize($tokens, $account, $userinfo, $client_name) {
+  $allowed_users = array('user1@example.com', 'user2@example.com');
+  // Allow only specific users to log in.
+  if (in_array($userinfo['email'], $allowed_users)) {
+    return TRUE;
+  }
+
+  // Block all others.
+  return FALSE;
+}
+
+/**
  * Perform an action after a successful authorization.
  *
  * @param array $tokens
